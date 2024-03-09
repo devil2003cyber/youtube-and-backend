213b41226acaf941da52fa5585195ad6ffb1d3cb
Rohan kadu <rkadu9359@gmail.com>
2024-03-10 01:05:46 +0530

    setup done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -54,6 +54,7 @@ this.password = await bcrypt.hash(this.password,10)
 next()
 })
 
+
 userSchema.methods.isPasswordCorrect = async function (password){
 return await bcrypt.compare(password,this.password)
 }
